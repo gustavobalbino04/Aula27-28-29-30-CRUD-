c05bcc8a7262a21802bb340d6988bbe4d4e9f2bf
gustavobalbino04 <gustavobalbino04>
2020-07-17 19:47:03 -0300

    ler feito

--- Produto.cs
+++ Produto.cs
@@ -1,10 +1,14 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Aula27_28_29_30
 {
     public class Produto
     {
        
+        
         public int Codigo { get; set; }
         public string Nome { get; set; }
         public float Preco { get; set; }
@@ -19,13 +23,51 @@ namespace Aula27_28_29_30
                 File.Create(PATH).Close();
             }
         }
+        /// <summary>
+        /// preparador das linhas do CVS
+        /// </summary>
+        /// <param name="produto"></param>
+        /// <returns>Retorna no CSV a linha organizada </returns>
         private string PrepararLinhaCSV(Produto produto){
             return $"codigo={produto.Codigo}-Nome {produto.Nome}-Preço R$ {produto.Preco}";
         }
+        /// <summary>
+        /// inserir
+        /// </summary>
+        /// <param name="p">inseri as informaçoes no CVS</param>
         public void Inserir(Produto p){
             var linha = new string[]{p.PrepararLinhaCSV(p)};
             File.AppendAllLines(PATH, linha);
         }
-  
-    }
+        public List<Produto> Ler(){
+
+
+        List<Produto> produtos = new List<Produto>();
+
+        string[] linhas = File.ReadAllLines(PATH);
+
+        foreach(var linha in linhas){
+
+            string[] dados = linha.Split(";");
+
+            Produto pro = new Produto();
+            pro.Codigo = Int32.Parse(SepararDados(dados[0]));
+            pro.Nome = SepararDados(dados[1]);
+            pro.Preco = Int32.Parse(SepararDados(dados[2]));
+
+            produtos.Add(pro);
+        }
+            produtos = produtos.OrderBy(z => z.Nome).ToList();
+
+            return produtos;
+
+        }
+        public string SepararDados(string dados)
+        {
+        
+            return dados.Split("=")[1];
+        }
+
+        
+      }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Aula27_28_29_30
 {
@@ -12,6 +13,13 @@ namespace Aula27_28_29_30
             produto.Preco = 399.99f;
 
             produto.Inserir(produto);
+
+            List<Produto> lista = new List<Produto>();
+            lista = produto.Ler();
+
+            foreach(Produto item in lista){
+                Console.WriteLine($"R${item.Preco} - Nome{item.Nome}");
+            }
 
         }
     }
